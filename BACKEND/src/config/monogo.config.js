2e296ed57038fd315cd305f0d16ee0bea4c7c5b3
amanupadhyay3108 <amanupadhyay3108@gmail.com>
2025-05-21 01:13:17 +0530

    Removed deprecated MongoDB options from connectDB

--- BACKEND/src/config/monogo.config.js
+++ BACKEND/src/config/monogo.config.js
@@ -3,10 +3,7 @@ console.log(process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
@@ -15,3 +12,18 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
