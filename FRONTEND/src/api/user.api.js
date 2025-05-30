c27697df6de552105f680db73b0d44e82504a1b3
amanupadhyay3108 <amanupadhyay3108@gmail.com>
2025-05-30 20:02:45 +0530

    axios error fix try

--- FRONTEND/src/api/user.api.js
+++ FRONTEND/src/api/user.api.js
@@ -1,19 +1,32 @@
 import axiosInstance from "../utils/axiosInstance"
 
-export const loginUser = async (password,email) =>{
-    const {data} = await axiosInstance.post("/api/auth/login",{email,password})
-    return data
-}
+export const loginUser = async (email, password) => {
+    const { data } = await axiosInstance.post(
+        "/api/auth/login",
+        { email, password },
+        { withCredentials: true } 
+    );
+    return data;
+};
 
-export const registerUser = async (name,password,email) =>{
-    const {data} = await axiosInstance.post("/api/auth/register",{name,email,password})
-    return data
-}
+export const registerUser = async (name, email, password) => {
+    const { data } = await axiosInstance.post(
+        "/api/auth/register",
+        { name, email, password },
+        { withCredentials: true } 
+    );
+    return data;
+};
 
-export const logoutUser = async () =>{
-    const {data} = await axiosInstance.post("/api/auth/logout")
-    return data
-}
+export const logoutUser = async () => {
+    const { data } = await axiosInstance.post(
+        "/api/auth/logout",
+        {}, 
+        { withCredentials: true } 
+    );
+    return data;
+};
+
 
 export const getCurrentUser = async () =>{
     const {data} = await axiosInstance.get("/api/auth/me")
